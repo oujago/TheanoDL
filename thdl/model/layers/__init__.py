96b037acc66baf15e1422bacfdd5ea42b91738c4
chaoming <oujago@gmail.com>
2017-04-04 23:17:04 +0800

    modify pooling.py

--- thdl/model/layers/__init__.py
+++ thdl/model/layers/__init__.py
@@ -6,3 +6,10 @@ from .base import Layer
 # basic
 from .basic import Dense
 from .basic import Softmax
+
+# convolution
+from .convolution import Convolution
+
+# pooling
+from .pooling import Pooling
+
--- thdl/model/layers/pooling.py
+++ thdl/model/layers/pooling.py
@@ -5,30 +5,46 @@ from theano.tensor.signal import pool
 from .base import Layer
 
 
-class Pool2D(Layer):
-    def __init__(self, pool_size, pad=(0, 0), ignore_border=True, mode='max'):
-        super(Pool2D, self).__init__()
+class Pooling(Layer):
+    def __init__(self, pool_size, pad=(0, 0), ignore_border=True, mode='max', stride=None):
+        super(Pooling, self).__init__()
 
         self.pool_size = pool_size  # (vertical ds, horizontal ds)
         self.pad = pad
         self.ignore_border = ignore_border
         self.mode = mode
+        self.stride = stride
 
-    def __call__(self, input):
-        # output
+    def connect_to(self, pre_layer=None):
+        assert pre_layer is not None
+        assert 4 >= len(pre_layer.out_shape) >= 3
+
+        old_h = pre_layer.out_shape[-2] + self.pad[0]
+        old_w = pre_layer.out_shape[-1] + self.pad[1]
+        pool_h, pool_w = self.pool_size
+
+        if self.stride is None:
+            new_h = old_h // pool_h
+            new_w = old_w // pool_w
+            assert old_h % pool_h == old_w % pool_w == 0
+        else:
+            new_h = (old_h - pool_h + 1) // self.stride[0] + 1
+            new_w = (old_w - pool_w + 1) // self.stride[1] + 1
+
+        self.out_shape = pre_layer.out_shape[:-2] + (new_h, new_w)
+
+    def forward(self, input, **kwargs):
         output = pool.pool_2d(input=input, ws=self.pool_size, pad=self.pad,
-                              ignore_border=self.ignore_border, mode=self.mode)
-
+                              ignore_border=self.ignore_border, mode=self.mode,
+                              stride=self.stride)
         return output
-
-    def __str__(self):
-        return 'Pool2D'
 
     def to_json(self):
         config = {
             'pool_size': self.pool_size,
             'pad': self.pad,
             'ignore_border': self.ignore_border,
-            'mode': self.mode
+            'mode': self.mode,
+            'stride': self.stride,
         }
         return config
