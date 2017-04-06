be8f1e363a89da39a6c3f4a3d03e02d59c72025d
chaoming <oujago@gmail.com>
2017-04-06 10:39:06 +0800

    improve Data class

--- thdl/data/main.py
+++ thdl/data/main.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 
 
+import numpy as np
+from thdl.utils import random
+
+
 class Data:
+    def __init__(self, shuffle=True, shuffle_seed=None):
+        self.shuffle = shuffle
+        self.shuffle_seed = shuffle_seed
+        if shuffle_seed:
+            self.shuffle_rng = np.random.RandomState(seed=shuffle_seed)
+        else:
+            self.shuffle_rng = random.get_rng().randint(1000, 10000000000)
 
     def get_xs(self):
         raise NotImplementedError("Please implement 'get_xs' method.")
@@ -10,9 +21,21 @@ class Data:
         raise NotImplementedError("Please implement 'get_ys' method.")
 
     def to_json(self):
-        raise NotImplementedError("Please implement 'to_json' method.")
+        config = {
+            'shuffle': self.shuffle,
+            "shuffle_seed": self.shuffle_seed,
+        }
+        return config
 
     @classmethod
     def from_config(cls, config):
         return cls(**config)
+
+    def shuffle_data(self, xs, ys):
+        if self.shuffle:
+            s = self.shuffle_rng.randint(0, 99999)
+            np.random.seed(s)  # definitely important
+            np.random.shuffle(xs)
+            np.random.seed(s)  # definitely important
+            np.random.shuffle(ys)
 
